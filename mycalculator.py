b1ac8d6935b8646b00a89604eed03230fbb5bafe
Ivyliz25 <kish.liz10349@gmail.com>
2025-08-05 15:49:15 +0300

    Ivy's Calculator

---
+++ mycalculator.py
@@ -0,0 +1,36 @@
+#Let make our first calculator...Yaay!
+#First assign two numbers
+num_1 = float(input("Enter the first number"))
+num_2 = float(input("Enter the second number"))
+
+#We're going to be a little extra and incorporate all calculations you could possibly think of
+#Addition ➕
+sum_result = num_1 + num_2
+#Subtraction ➖
+difference_result = num_1 - num_2
+#Division ➗ 
+quotient_result = num_1 / num_2
+#Multiplication ✖️
+product_result = num_1 * num_2
+#Floor Division
+floor_result = num_1 // num_2
+#Remainder
+remainder_result = num_1 % num_2
+#Power
+power_result = num_1 ** num_2
+
+#Ok now let's see if we're on to something
+print(f"Results of the two numbers")
+print(f"Sum: {sum_result}")
+print(f"difference: { difference_result}")
+print(f"quotient: {quotient_result}")
+print(f"multiplication: {product_result}")
+print(f"floor: {floor_result}")
+print(f"remainder: {remainder_result}")
+print(f"power: {power_result}")
+
+#Time to finally see if we've actually got it😂😂
+
+#You've actually got it!!!! 🥳🥳🥳🎉🎉   🔛🔝
+#You'll better start getting my autograph cause I'm 'bout to be Blaise Pascal Jr if yk what I'm talking about😏🫴🏼
+#Clarke it 🤏🏽🤏🏽🤏🏽🤏🏽
